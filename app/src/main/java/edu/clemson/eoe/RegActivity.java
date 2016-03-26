ffd25f89f318019d985601b37db370664626ebf7
Raghuvaran <rchowda@clemson.edu>
2016-03-26 00:50:00 -0400

    Registration activity semi-UI

--- app/src/main/java/edu/clemson/eoe/RegActivity.java
+++ app/src/main/java/edu/clemson/eoe/RegActivity.java
@@ -1,5 +1,7 @@
 package edu.clemson.eoe;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class RegActivity extends AppCompatActivity {
+
+    Button dobButton;
+    int year_DOB, month_DOB, day_DOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +27,50 @@ public class RegActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initializing DOB with today's values
+        Calendar calendar = Calendar.getInstance();
+        year_DOB= calendar.get(Calendar.YEAR);
+        month_DOB= calendar.get(Calendar.MONTH);
+        day_DOB= calendar.get(Calendar.DAY_OF_MONTH);
+
+        //DOB button
+        dobButton = (Button) findViewById(R.id.DOBbutton);
+        dobButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(1);
+            }
+        });
+
+
+
+
 
     }
+
+    /**
+     * Creates a new dialog, here it is used to create a pop-up date picker dialog
+     * @param id is 1 for the datepicker dialog
+     * @return the datepicker dialog
+     */
+    @Override
+    protected Dialog onCreateDialog(int id){
+        if(id == 1){
+            //Create a Datepicker Dialog on click of DOB button
+            return new DatePickerDialog(this, dpListener, year_DOB,month_DOB,day_DOB );
+        }
+        return null;
+    }
+    //Assigns the selected date values to the declared variables
+    protected DatePickerDialog.OnDateSetListener dpListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_DOB=year;
+            month_DOB=monthOfYear+1; //By default month starts from January =0
+            day_DOB=dayOfMonth;
+            dobButton.setText("DOB : "+month_DOB+"/"+day_DOB+"/"+year_DOB);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
