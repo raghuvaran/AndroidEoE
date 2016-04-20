a6ac5caab9690fdca2623c490fa65e9f30044aa1
Santhosh raj Ravirala <santh@LAPTOP-DC5PMQ8K.(none)>
2016-04-19 21:05:14 -0400

    About activity

--- app/src/main/java/edu/clemson/eoe/About.java
+++ app/src/main/java/edu/clemson/eoe/About.java
@@ -1,11 +1,21 @@
 package edu.clemson.eoe;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -21,10 +31,70 @@ public class About extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     public static class MyPreferenceFragment extends PreferenceFragment {
+        PopupWindow popUp;
+        LinearLayout layout;
+        TextView tv;
+        LinearLayout.LayoutParams params;
+        ScrollView scroll;
+        LinearLayout mainLayout;
+        Button but;
+        boolean click = true;
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings);
+
+            addPreferencesFromResource(R.xml.about);
+
+            Preference myPref = (Preference) findPreference("Contact_key");
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    //open browser or intent here
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_EMAIL, "sravira@clemson.edu");
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                    intent.putExtra(Intent.EXTRA_TEXT, "I'm email body.");
+
+                    startActivity(Intent.createChooser(intent, "Send Email"));
+                    return true;
+                }
+            });
+
+            Preference summary = (Preference) findPreference("Summary_key");
+            popUp = new PopupWindow(getActivity());
+            popUp.setBackgroundDrawable(null);
+            //popUp.
+            layout = new LinearLayout(getActivity());
+            mainLayout = new LinearLayout(getActivity());
+            tv = new TextView(getActivity());
+            scroll =new ScrollView(getActivity());
+            summary.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    if (click) {
+                        popUp.showAtLocation(mainLayout, Gravity.CENTER, 100, 100);
+                        popUp.update(50, 50, -1, -1);
+
+                        click = false;
+                    } else {
+                        popUp.dismiss();
+                        click = true;
+                    }
+                    return true;
+                }
+            });
+            params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            tv.setText("Hi this is a sample text for popup window");
+            layout.addView(tv, params);
+            popUp.setContentView(layout);
+
+            // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+            //mainLayout.addView(but, params);
+            //setContentView(mainLayout);
+
+
         }
     }
     /**
